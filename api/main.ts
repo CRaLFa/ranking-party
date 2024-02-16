79fdc150be56c9bbb127143695ee25bcb651ce8a
rana <minami.smd@gmail.com>
2024-02-16 23:30:05 +0900

    create API

--- api/main.ts
+++ api/main.ts
@@ -1,27 +1,53 @@
-import { Application, Router } from "https://deno.land/x/oak@v13.2.5/mod.ts";
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" with { type: "json" };
+import { Application, Router } from 'https://deno.land/x/oak@v13.2.5/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
+
+type Data = {
+  [no: string]: {
+    [key: string]: string | {
+      [key: string]: string | number
+    };
+  };
+};
+
+type PostData = {
+  member: string;
+  foods: {
+    [rank: number]: string;
+  }[];
+  voted: number[];
+};
+
+const getData = async () => JSON.parse(await Deno.readTextFile('./api/data.json')) as Data;
 
 const router = new Router();
 
 router
-  .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
+  .get('/data', async (ctx) => {
+    const data = await getData();
+    ctx.response.body = data;
   })
-  .get("/api", (context) => {
-    context.response.body = data;
-  })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const filtered = data.filter((item) =>
-        item["name"].toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (filtered.length === 0) {
-        context.response.body = "No dinosaurs found.";
-      } else {
-        context.response.body = filtered[0];
-      }
-    }
+  .post('/data', async (ctx) => {
+    const posted = await ctx.request.body.json() as PostData;
+    const data = await getData();
+
+    Object.keys(data).forEach(no => {
+      const num = parseInt(no, 10);
+
+      data[no][posted.member] = {
+        1: posted.foods[num][1],
+        2: posted.foods[num][2],
+        3: posted.foods[num][3],
+        voted: posted.voted[num],
+      };
+    });
+
+    Deno.writeTextFile('./api/data.json', JSON.stringify(data, null, 2))
+      .then(() => {
+        ctx.response.body = { succeeded: true };
+      })
+      .catch(() => {
+        ctx.response.body = { succeeded: false };
+      });
   });
 
 const app = new Application();
